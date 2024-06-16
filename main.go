27fa7b4acb197a5b9747f81df3f7fc15d9cac279
Kesit Budi Kusumo <kesit@kernel.id>
2024-06-16 14:36:35 +0700

    feat: add iterative solution

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := 5
+	output := generate(input)
+	fmt.Println(output)
+}
+
+func generate(numRows int) [][]int {
+	result := make([][]int, 0)
+	for i := 0; i < numRows; i++ {
+		element := make([]int, i+1)
+		element[0], element[i] = 1, 1
+		for j := 1; j < i; j++ {
+			element[j] = result[i-1][j-1] + result[i-1][j]
+		}
+		result = append(result, element)
+	}
+	return result
 }
